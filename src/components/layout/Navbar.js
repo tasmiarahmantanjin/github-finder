22581340f3a405283bef3f36b30e00a8d473ddaa
Tasmia Rahman <tasmiata@gmail.com>
2021-02-04 10:58:47 +0200

    new update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 
 const Navbar = ({ icon, title }) => {
 		return (
-			<nav className="navbar bg-primary">
+			<nav className="navbar bg-dark">
 				<h1>
-					<i className={icon}/>
-					{title}</h1>
+					<i className={icon}/>{title}
+				</h1>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
 			</nav>
 		)
 	}
 
 Navbar.defaultProps = {
-	title: 'Github Finder',
+	title: ' Github Finder',
 	icon: 'fab fa-github'
 };
 
