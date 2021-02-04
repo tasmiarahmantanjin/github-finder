3540bc8bcb87c452d58effcdbfa2df29518a1eaf
Tasmia Rahman <tasmiata@gmail.com>
2021-02-04 10:57:58 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { Component }from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 // import UserItem from "./components//users/Useritem";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from 'axios'
 
 import './App.css';
@@ -10,18 +14,21 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    user: {},
+    repos: [],
+    loading: false,
+    alert: null,
   }
-
+  
   // To Display some default profile just to look good
   async componentDidMount() {
     this.setState({ loading: true })
-  
+
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}?client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({users: res.data, loading: false});
+    this.setState({ users: res.data, loading: false });
   }
-  
+
   // Search Github Users
   searchUsers = async text => {
     this.setState({ loading: true })
@@ -31,15 +38,74 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  // Get single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}?client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  } 
+
+  // Get Users Repo
+  getUserRepos = async username => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}?client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
+  // Clear users form states
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert 
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
+
   render() {
+    const { users, user, repos, loading } = this.state;
+
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users}/>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User
+                  { ...props }
+                  getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading} />
+              )} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
